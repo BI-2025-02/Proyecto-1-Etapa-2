5ef77f5983a1450a2c8bda30892cbb975f02df7e
Diego Molano <d.molanor@uniandes.edu.co>
2025-10-09 19:36:11 -0500

    Primera versión Pipeline y API. Falta prompting

---
+++ model_pipeline.py
@@ -0,0 +1,35 @@
+import os
+
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+CURRENT_MODEL_PATH = "models/model_current.joblib"
+CURRENT_VECTORIZER_PATH = "models/tfidf_current.joblib"
+
+def build_pipeline():
+    return Pipeline([
+        ("tfidf", TfidfVectorizer(
+            ngram_range=(1,2), 
+            min_df=3, 
+            max_df=0.9, 
+            sublinear_tf=True, 
+            norm="l2"
+        )),
+        ("clf", LogisticRegression(
+            max_iter=2000, 
+            class_weight="balanced", 
+            random_state=42,
+            solver="lbfgs"
+        ))
+    ])
+
+def load_current_model():
+    model = joblib.load(CURRENT_MODEL_PATH)
+    vectorizer = joblib.load(CURRENT_VECTORIZER_PATH)
+    return model, vectorizer
+
+def save_model(model, vectorizer, path_model=CURRENT_MODEL_PATH, path_vec=CURRENT_VECTORIZER_PATH):
+    joblib.dump(model, path_model)
+    joblib.dump(vectorizer, path_vec)
